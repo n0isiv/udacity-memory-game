f9f9a204cec39b0dbbe3fb2a8a1894688b61058f
Niels Grieger <niels.grieger@pm.me>
2018-12-06 13:39:53 +0100

    feat: add stats and winning screen

--- js/helpers.js
+++ js/helpers.js
@@ -1,6 +1,10 @@
 /**
  * Randomly shuffle the given array using the Fisher-Yates algorithm.
  * https://stackoverflow.com/a/2450976/1293256
+ *
+ * @param {any[]} array The array to shuffle
+ *
+ * @returns {any[]} The shuffled array
  */
 function shuffle(array) {
   var currentIndex = array.length;
@@ -24,6 +28,11 @@ function shuffle(array) {
 /**
  * Create matrix from the given list.
  * https://stackoverflow.com/a/4492417
+ *
+ * @param {any[]} list List to be converted to a matrix
+ * @param {number} elementsPerSubArray Number of elements per sub-array
+ *
+ * @returns {any[][]} The generated matrix
  */
 function listToMatrix(list, elementsPerSubArray) {
   var matrix = [],
@@ -43,9 +52,32 @@ function listToMatrix(list, elementsPerSubArray) {
 }
 
 /**
- * Generates 4 rows with 4 icons each.
+ * Remove all child nodes from the element.
  *
- * You must üass in an array with 8 icons.
+ * @param {HTMLElement} element
+ */
+function removeAllChildren(element) {
+  while (element.firstChild) {
+    element.removeChild(element.firstChild);
+  }
+}
+
+/**
+ * Update the inner text of the given element
+ *
+ * @param {HTMLElement} element
+ * @param {string} text
+ */
+function updateInnerText(element, text) {
+  element.innerText = text;
+}
+
+/**
+ * Generates 4 rows with 4 icons each
+ *
+ * @param {string[]} icons List of icon names (must be 8)
+ *
+ * @returns {string[][]} The generated rows
  */
 function generateRows(icons) {
   let rows = [];
@@ -66,19 +98,27 @@ function generateRows(icons) {
  * Takes the rows generated with `generateRows`
  * and puts them in a document fragment.
  */
+
+/**
+ * Puts the rows generated with the `generateRows` function in a document fragment.
+ *
+ * @param {string[][]} rows
+ *
+ * @returns {DocumentFragment} The generated document fragment
+ */
 function renderRowsInFragment(rows) {
   const fragment = document.createDocumentFragment();
 
   rows.forEach(row => {
     const rowElm = document.createElement("div");
 
-    rowElm.classList.add("row");
+    rowElm.classList.add("wrapper__row");
 
     row.forEach(card => {
       const cardElm = document.createElement("div");
       const iconElm = document.createElement("i");
 
-      cardElm.classList.add("memory-card");
+      cardElm.classList.add("wrapper__card");
       iconElm.classList.add(`icon-${card}`, "icon--invisible");
       iconElm.dataset.icon = card;
 
@@ -92,6 +132,162 @@ function renderRowsInFragment(rows) {
   return fragment;
 }
 
+/**
+ * Sets the 'icon--invisible` class on the given `iconElms`.
+ *
+ * @param  {...HTMLElement} iconElms
+ */
 function hideIconElements(...iconElms) {
   iconElms.forEach(iconElm => iconElm.classList.add("icon--invisible"));
+}
+
+/**
+ * Convert the given `seconds` to a 'mm:ss' string.
+ *
+ * @param {number} seconds
+ */
+function generateTimerText(seconds) {
+  const min = Math.floor(seconds / 60);
+  const sec = seconds % 60;
+
+  return `${min.toString().padStart(2, "0")}:${sec
+    .toString()
+    .padStart(2, "0")}`;
+}
+
+/**
+ * Generate the star rating in a document fragment.
+ *
+ * 5 stars = < 11 moves
+ * 4 stars = < 13 moves
+ * 3 stars = < 15 moves
+ * 2 stars = < 17 moves
+ * 1 star = >= 17 moves
+ *
+ * @param {number} counter
+ *
+ * @returns {DocumentFragment} The generated document fragment
+ */
+function getStarRatingFragment(counter) {
+  let rating = 1;
+
+  const fragment = document.createDocumentFragment();
+  const starIconFullElm = document.createElement("i");
+  const starIconEmptyElm = document.createElement("i");
+
+  starIconFullElm.classList.add("icon-star");
+  starIconEmptyElm.classList.add("icon-star-empty");
+
+  if (counter < 11) {
+    rating = 5;
+  } else if (counter < 13) {
+    rating = 4;
+  } else if (counter < 15) {
+    rating = 3;
+  } else if (counter < 17) {
+    rating = 2;
+  }
+
+  for (let i = 0; i < rating; i++) {
+    fragment.appendChild(starIconFullElm.cloneNode(true));
+  }
+
+  for (let i = 0; i < 5 - rating; i++) {
+    fragment.appendChild(starIconEmptyElm.cloneNode(true));
+  }
+
+  return fragment;
+}
+
+/**
+ * Shows the winning screen.
+ *
+ * @param {HTMLElement} gameElm
+ * @param {HTMLElement} statsElm
+ * @param {HTMLElement} wrapperElm
+ * @param {HTMLElement} winningScreenElm
+ * @param {HTMLElement} timerElm
+ * @param {HTMLElement} counterElm
+ * @param {HTMLElement} ratingElm
+ * @param {number} seconds
+ * @param {number} counter
+ */
+function renderWinningScreen(
+  gameElm,
+  statsElm,
+  wrapperElm,
+  winningScreenElm,
+  timerElm,
+  counterElm,
+  ratingElm,
+  seconds,
+  counter
+) {
+  gameElm.style.display = "none";
+  statsElm.style.display = "none";
+  wrapperElm.style.display = "none";
+
+  winningScreenElm.style.display = "flex";
+
+  // set timer text
+  updateInnerText(timerElm, generateTimerText(seconds));
+  // set counter text
+  updateInnerText(counterElm, counter);
+
+  // set star rating
+  removeAllChildren(ratingElm);
+  ratingElm.appendChild(getStarRatingFragment(counter));
+
+  gameElm.style.display = "block";
+}
+
+/**
+ * Return the inital values for the stats.
+ *
+ * @returns {any[]} The initial values
+ */
+function initializeStats() {
+  return [[], 0, 0, null, []];
+}
+/**
+ * Initialize the game.
+ *
+ * @param {HTMLElement} gameElm
+ * @param {HTMLElement} statsElm
+ * @param {HTMLElement} wrapperElm
+ * @param {HTMLElement} timerElm
+ * @param {HTMLElement} counterElm
+ * @param {HTMLElement} ratingElm
+ * @param {HTMLElement} winningScreenElm
+ * @param {HTMLElement} icons
+ */
+function initializeGame(
+  gameElm,
+  statsElm,
+  wrapperElm,
+  timerElm,
+  counterElm,
+  ratingElm,
+  winningScreenElm,
+  icons
+) {
+  const rows = generateRows(icons);
+  const fragment = renderRowsInFragment(rows);
+
+  gameElm.style.display = "none";
+  winningScreenElm.style.display = "none";
+
+  removeAllChildren(wrapperElm);
+  wrapperElm.appendChild(fragment);
+  wrapperElm.style.display = "flex";
+
+  updateInnerText(timerElm, generateTimerText(0));
+
+  updateInnerText(counterElm, "0");
+
+  removeAllChildren(ratingElm);
+  ratingElm.appendChild(getStarRatingFragment(0));
+
+  statsElm.style.display = "block";
+  gameElm.style.display = "block";
 }
--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
-// -+-----------------------------+-
-// CONSTANTS
-// -+-----------------------------+-
+//
+// ─── CONSTANTS ──────────────────────────────────────────────────────────────────
+//
 
+// the array must allways contain 8 icons
 const AVAILABLE_ICONS = [
   "award",
   "leaf",
@@ -14,23 +15,51 @@ const AVAILABLE_ICONS = [
 ];
 
 // DOM ELEMENTS
+const GAME = document.querySelector(".game");
 const WRAPPER = document.querySelector(".wrapper");
+const STATS = document.querySelector(".stats");
+const TIMER = document.querySelector(".stats__timer");
+const COUNTER = document.querySelector(".stats__counter");
+const STARRATING = document.querySelector(".stats__rating");
+const WINNINGSCREEN = document.querySelector(".winning-screen");
+const WINNINGSCREENTIMER = document.querySelector(".winning-screen__timer");
+const WINNINGSCREENCOUNTER = document.querySelector(".winning-screen__counter");
+const WINNINGSCREENRATING = document.querySelector(".winning-screen__rating");
+const RETRY = document.querySelector(".winning-screen__retry");
 
-// -+-----------------------------+-
-// LOGIC
-// -+-----------------------------+-
+//
+// ─── LOGIC ──────────────────────────────────────────────────────────────────────
+//
 
-let clickedIcons = [];
-const matchedIcons = [];
-const rows = generateRows(AVAILABLE_ICONS);
-const fragment = renderRowsInFragment(rows);
+// STATS
+let clickedIcons;
+let counter;
+let seconds;
+let timer;
+let matchedIcons;
 
-WRAPPER.appendChild(fragment);
+// set initial stat values and initialize the game (stats + memory cards)
+[clickedIcons, counter, seconds, timer, matchedIcons] = initializeStats();
+initializeGame(
+  GAME,
+  STATS,
+  WRAPPER,
+  TIMER,
+  COUNTER,
+  STARRATING,
+  WINNINGSCREEN,
+  AVAILABLE_ICONS
+);
+
+//
+// ─── GAME LOGIC ─────────────────────────────────────────────────────────────────
+//
 
 WRAPPER.addEventListener("click", function(ev) {
+  // the clicked icon element
   let iconElm;
 
-  if (ev.target.className === "memory-card") {
+  if (ev.target.className === "wrapper__card") {
     // user clicked on a memory card
     iconElm = ev.target.firstChild;
   } else if (ev.target.className.startsWith("icon")) {
@@ -41,19 +70,28 @@ WRAPPER.addEventListener("click", function(ev) {
     return;
   }
 
-  // ignore clicks on revealed card pairs
+  if (timer === null) {
+    timer = setInterval(() => {
+      seconds += 1;
+      text = generateTimerText(seconds);
+
+      updateInnerText(TIMER, text);
+    }, 1000);
+  }
+
+  // ignore clicks on matched cards
   if (matchedIcons.includes(iconElm.dataset.icon)) {
     return;
   }
 
-  // only allow 2 cards to be revealed at the same time
+  // only allow 2 cards to be visible at the same time
   if (clickedIcons.length === 2) {
     return;
   }
 
-  // make icon visible
+  // make the icon visible
   iconElm.classList.remove("icon--invisible");
-  // store clicked icon
+  // store the clicked icon
   clickedIcons.push(iconElm);
 
   // user selected 2 cards, check if they match
@@ -64,15 +102,20 @@ WRAPPER.addEventListener("click", function(ev) {
 
       return;
     }
+
     const iconName1 = clickedIcons[0].dataset.icon;
     const iconName2 = clickedIcons[1].dataset.icon;
 
     if (iconName1 === iconName2 && clickedIcons[0] !== clickedIcons[1]) {
+      // the selected cards match
       matchedIcons.push(iconName1);
 
       // reset clicked icons
       clickedIcons = [];
     } else {
+      // the selected cards do not match
+
+      // wait 750ms before hiding the cards
       setTimeout(() => {
         hideIconElements(...clickedIcons);
 
@@ -80,5 +123,52 @@ WRAPPER.addEventListener("click", function(ev) {
         clickedIcons = [];
       }, 750);
     }
+
+    // update the counter
+    counter += 1;
+    updateInnerText(COUNTER, counter);
+
+    // update the star rating
+    removeAllChildren(STARRATING);
+    STARRATING.appendChild(getStarRatingFragment(counter));
+
+    // user has uncovered all card pairs
+    if (matchedIcons.length === 8) {
+      // stop timer and reset the active state
+      clearInterval(timer);
+      timer = null;
+
+      // switch to winning screen
+      renderWinningScreen(
+        GAME,
+        STATS,
+        WRAPPER,
+        WINNINGSCREEN,
+        WINNINGSCREENTIMER,
+        WINNINGSCREENCOUNTER,
+        WINNINGSCREENRATING,
+        seconds,
+        counter
+      );
+    }
   }
+});
+
+//
+// ─── RETRY LOGIC ───────────────────────────────────────────────────────────────
+//
+
+RETRY.addEventListener("click", function(ev) {
+  // reset stats and switch back to the game
+  [clickedIcons, counter, seconds, timer, matchedIcons] = initializeStats();
+  initializeGame(
+    GAME,
+    STATS,
+    WRAPPER,
+    TIMER,
+    COUNTER,
+    STARRATING,
+    WINNINGSCREEN,
+    AVAILABLE_ICONS
+  );
 });
